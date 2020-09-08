3822ae83cbae8e29875ef7d192406835c84182b4
안정은 <jeong.dev01@gmail.com>
2020-09-08 20:04:43 +0900

    teleporting...

---
+++ New Unity Project1/Assets/Scripts/Portal.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Portal : MonoBehaviour
+{
+    private bool canMove;
+
+    public Vector2 position = new Vector2();
+    public GameObject door;
+    public GameObject player;
+
+    public Portal()
+    {
+        
+    }
+
+    void Start()
+    {
+        Debug.Log("portal script is loaded");
+        this.door = GameObject.FindGameObjectWithTag("mainDoor");
+    }
+
+    void Update()
+    {
+        canMove = false;
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            
+            if(canMove)
+            {
+                Debug.Log("canMove " + canMove);
+                SceneManager.LoadScene("enterHome");
+            }
+        }
+
+        //if (Input.GetKey(KeyCode.UpArrow))
+        //{
+
+
+        //}
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.transform.tag == "player")
+        {
+            Debug.Log("on trigger");
+            //door.transform.position = new Vector2(position.x, position.y);
+            canMove = true;
+        }
+
+
+     }
+
+    public void SetInitPosition()
+    {
+        this.transform.position = position;
+    }
+}
